515dc1ea2690093aa4114c6c39111899bd6f2d29
vanh2001 <anhviet1709@gmail.com>
2022-02-08 14:42:53 +0700

    Add bai chuong trinh menu

---
+++ drawing_menu/src/Main.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int choice = -1;
+        Scanner sc = new Scanner(System.in);
+        while (choice != 0){
+            //Thiết kế menu và cho người dùng chọn
+            System.out.println("Menu");
+            System.out.println("1. Draw the triangle");
+            System.out.println("2. Draw the square");
+            System.out.println("3. Draw the rectangle");
+            System.out.println("0. Exit");
+            System.out.print("Enter your choice: ");
+            choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Draw the triangle");
+                    System.out.println("******");
+                    System.out.println("*****");
+                    System.out.println("****");
+                    System.out.println("***");
+                    System.out.println("**");
+                    System.out.println("*");
+                    break;
+                case 2:
+                    System.out.println("Draw the square");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    break;
+                case 3:
+                    System.out.println("Draw the rectangle");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    break;
+                case 0:
+                    //Thoát khỏi chương trình
+                    System.exit(0);
+                    System.out.println("Exit ");
+                default:
+                    System.out.println("No choice!");
+            }
+        }
+
+    }
+}
